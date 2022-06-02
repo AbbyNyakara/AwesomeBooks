d28d4e0d17a4854cb81352bd87db52ba63a58b9d
Abby Nyakara <abbynyakara@gmail.com>
2022-06-02 11:51:57 +0300

    create one page website

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const title = document.querySelector('.title-input');
 const author = document.querySelector('.author-input');
 const loggedBooks = document.querySelector('.logged-books');
 const addButton = document.querySelector('.add-book');
+const contact = document.querySelector('.contact');
+const form = document.querySelector('form');
+const currentDate = document.querySelector('.date');
+const pageHeading = document.querySelector('main-heading');
+
+// Select the Links to the pages 
+const bookLink = document.querySelector('nav ul li .book-link');
+const formLink = document.querySelector('nav ul li .form-link');
+const contactLink = document.querySelector('nav ul li .contact-link');
 
 let books = [];
 
@@ -11,13 +20,13 @@ function showBook() {
   loggedBooks.innerHTML = '';
   for (let i = 0; i < books.length; i += 1) {
     loggedBooks.innerHTML += `
-    <div>
-      <p class="title">${books[i].title}</p>
-      <p class="author">${books[i].author}</p>
-      <button class="button" onclick="removeBook(${i})">remove</button>
-      <hr/>
-    </div>
+        <div class="card">
+            <p class="title">${books[i].title} by ${books[i].author}</p>
+            <button class="button" onclick="removeBook(${i})">Remove</button>
+        </div>
    `;
+
+   // Reset the values to null to allow the user to input again
     title.value = '';
     author.value = '';
   }
@@ -33,6 +42,10 @@ window.onload = () => {
   if (localStorage.getItem('books')) {
     books = JSON.parse(localStorage.getItem('books'));
   }
+
+  contact.classList.add('hide');
+  loggedBooks.classList.add('hide');
+
   showBook();
 };
 
@@ -44,4 +57,33 @@ addButton.addEventListener('click', () => {
   books.push(book);
   showBook();
   localStorage.setItem('books', JSON.stringify(books));
+});
+
+const today = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
+
+currentDate.innerHTML = `
+  <p>${today}</p>
+`;
+
+// Add Event listeners for one page website 
+
+formLink.addEventListener('click', ()=>{
+  loggedBooks.style.display = 'none';
+  form.style.display = 'grid';
+  contact.style.display = 'none';
+  pageHeading.innerHTML = 'Add book';
+});
+
+bookLink.addEventListener('click', ()=>{
+  loggedBooks.style.display = 'none';
+  form.style.display = 'none';
+  loggedBooks.style.display = 'block';
+  pageHeading.innerHTML = 'Display all books';
+});
+
+contactLink.addEventListener('click', ()=>{
+  contact.style.display = 'grid';
+  form.style.display = 'none';
+  loggedBooks.style.display = 'none';
+  pageHeading.innerHTML = 'Contact Information';
 });
